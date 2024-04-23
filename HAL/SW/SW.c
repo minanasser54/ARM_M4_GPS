2d2877a735ba00737cd849a0c00bae9d7aa36f64
zyadmagdy11 <zyadmagdy32@gmail.com.com>
2024-04-23 22:26:50 +0200

    Switch1

---
+++ HAL/SW/SW.c
@@ -0,0 +1,31 @@
+
+
+#include "../../MCAL/GPIO/DIO_Driver.h"
+#include "SW.h"
+
+
+
+unsigned char button_in;
+unsigned char button_prev = 0x10;
+
+
+DIO_vPORTINIT("F");
+DIO_vSETPINDIR("F",4,0);
+
+
+unsigned char SW1_Input(void)
+{
+    return GPIO_PORTF_DATA_R &0x10;
+}
+
+bool SW1_ispressed(void)
+{
+    button_in = SW1_Input();                        
+    if (button_prev != 0x10 && button_in == 0x10)   //rising edge
+        return true;
+    else 
+        return false;
+
+    button_prev = button_in;
+
+}
---
+++ HAL/SW/SW.h
@@ -0,0 +1,16 @@
+
+#ifndef SW
+#define SW
+#include <stdbool.h>
+#include <stdio.h>
+
+
+
+unsigned char SW1_Input(void);
+bool SW1_ispressed(void);
+
+
+
+
+#endif
+
