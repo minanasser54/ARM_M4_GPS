0ab12a2abde071693248b1179fb52aadebec106f
zyadmagdy11 <zyadmagdy32@gmail.com.com>
2024-04-24 15:36:33 +0200

    Correct some errors

--- HAL/SW/SW.h
+++ HAL/SW/SW.h
@@ -1,14 +1,15 @@
 
-#ifndef SW
-#define SW
-#include <stdbool.h>
-#include <stdio.h>
-#include "../../MCAL/GPIO/DIO_Driver.h"
+#ifndef __SW__
+#define __SW__
 
+
+
+#define  SW1      0x10
+#define  SW2      0x01
 
 
 unsigned char SW1_Input(void);
-bool SW1_ispressed(void);
+bool          SW1_ispressed(void);
 
 
 
